c9339bda1aa54465263d25667ec7c7275fbfee76
npl22 <nli22@jhu.edu>
2017-04-18 11:09:37 -0700

    Add cats model and controller

---
+++ nine_nine_cats/app/models/cat.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+# TODO: age, validation for birth_date not in future
+class Cat < ApplicationRecord
+  validates :birth_date, :color, :name, :sex, :description, presence: true
+  validates :color, inclusion: ['Black', 'Orange', 'Grey', 'Multicolor']
+
+end
