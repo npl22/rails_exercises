bb9ff98978c100c7f8bd5ea7eb7b94d46b925a03
npl22 <nli22@jhu.edu>
2017-04-24 10:46:35 -0700

    Add gems, user model

---
+++ RedditOnRails/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, pw)
+    user = User.find_by(username: username)
+    return user if user.is_password?(pw)
+    nil
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
