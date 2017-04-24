bf465acc8a03515acf3301a506e86f95c30eaabf
npl22 <nli22@jhu.edu>
2017-04-24 11:20:34 -0700

    Add sign up page, errors partial

--- RedditOnRails/app/helpers/application_helper.rb
+++ RedditOnRails/app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  # this is ruby not erb!!!!
+  # the whole thing is a string that you call html_safe on
+  # escape quotes for type, name, value
+  # use string interpolation for form_authenticity_token b/c erb not ruby
+  # remember to close your input tag
+  # def auth_token
+  #   "<input type=\"hidden\"
+  #   name=\"authenticity token\"
+  #   value=\"#{form_authenticity_token}\">".html_safe
+  # end
+
+  # def auth_token
+  #   "<input
+  #     type=\"hidden\"
+  #     name=\"authenticity_token\"
+  #     value=\"#{form_authenticity_token}\"
+  #     >".html_safe
+  # end
+
+  def auth_token
+    "<input
+      type=\"hidden\"
+      name=\"authenticity_token\"
+      value=\"#{form_authenticity_token}\"
+      >".html_safe
+  end
 end
--- RedditOnRails/app/models/user.rb
+++ RedditOnRails/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
+  attr_reader :password
   validates :password, length: { minimum: 6 }, allow_nil: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   after_initialize :ensure_session_token
