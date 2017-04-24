7fcf9fe22972f5156480c5e36ad4beb089db0030
npl22 <nli22@jhu.edu>
2017-04-24 12:03:11 -0700

    Add Sign In Sign Out, Logout buttons, current_user functionality

--- RedditOnRails/app/controllers/application_controller.rb
+++ RedditOnRails/app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :signed_in?
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
   end
 
   def login!(user)
@@ -11,7 +15,16 @@ class ApplicationController < ActionController::Base
   end
 
   def logout!
-    current_user.try(reset_session_token!)
+    current_user.reset_session_token!
     session[:session_token] = nil
+  end
+
+  private
+
+  def require_signed_in
+    unless current_user
+      redirect_to new_session_url
+      flash[:errors] = ['Please log in first']
+    end
   end
 end
